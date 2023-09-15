5725b17ecb385c8d56aef69f68192d82fb48ce73
Kuncheria-KV <kuncheria@keyvalue.systems>
2023-09-16 00:11:01 +0530

    feat: add new human modal

--- src/js/containers/MemberProfile/MusculoSkeletal/App.js
+++ src/js/containers/MemberProfile/MusculoSkeletal/App.js
@@ -8,13 +8,13 @@ export default function Viewer() {
   return (
     <Canvas shadows dpr={[1, 2]} camera={{ fov: 50 }}>
       <Suspense fallback={null}>
-        <Stage controls={ref} preset="rembrandt" intensity={1}  environment={null}>
+        <Stage controls={ref} preset="rembrandt" intensity={1}  environment="city">
         false
           <Model />
         false
         </Stage>
       </Suspense>
-      <OrbitControls ref={ref} autoRotate />
+      <OrbitControls ref={ref} autoRotate rotateSpeed={8}/>
     </Canvas>
   )
 }
